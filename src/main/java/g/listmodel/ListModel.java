8a46df4ef7e2f6c80a593412f60a54464293c2ab
erkanzzafer <zzafererkann@gmail.com>
2024-02-19 14:37:32 +0300

    first commit

---
+++ src/main/java/g/listmodel/ListModel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package g.listmodel;
+
+/**
+ *
+ * @author zafer
+ */
+public class ListModel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
